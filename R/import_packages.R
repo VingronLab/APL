ea7673dd619732cc97493cc7abc69ac49109893f
Elzbieta Gralinska <gralinsk@molgen.mpg.de>
2021-11-28 16:26:56 +0100

    Move files to directories

---
+++ R/import_packages.R
@@ -0,0 +1,11 @@
+
+#' @import methods
+#' @import SummarizedExperiment
+#' @importFrom stats as.formula na.omit quantile runif var
+#' @importFrom utils head setTxtProgressBar txtProgressBar
+#' @importFrom ggplot2 ggplot aes geom_point guide_colorbar
+#' @importFrom topGO showSigOfNodes score
+#' @importFrom viridisLite viridis
+#' @importClassesFrom Seurat Seurat
+#' @importClassesFrom SingleCellExperiment SingleCellExperiment
+NULL
