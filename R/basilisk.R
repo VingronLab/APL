4154f894c0a6126b9e9ce2a83d5e1e7eda6b8118
Clemens Kohl <kohl.clemens@gmail.com>
2024-10-17 23:48:00 +0200

    added dependencies

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,6 +1,5 @@
-
 #' @importFrom basilisk BasiliskEnvironment
 APL_env <- BasiliskEnvironment(envname="APL-env",
     pkgname="APL",
-    packages=c("numpy==1.26.4", "pytorch==2.3.0")
+    packages=c("numpy==1.26.4", "pytorch==2.3.0", "libstdcxx-ng==14.2.0", "libprotobuf==4.25.3")
 )
