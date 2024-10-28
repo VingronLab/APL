b9c17fbddee5248392904d3fb24b4df4924e9897
Clemens Kohl <kohl.clemens@gmail.com>
2024-10-28 21:37:54 +0100

    Revert "increase pytorch version"
    This reverts commit 5a6b9f443cab8a296b62fbcb77837e74390cc9a2.

--- R/basilisk.R
+++ R/basilisk.R
@@ -2,5 +2,5 @@
 #' @importFrom basilisk BasiliskEnvironment
 APL_env <- BasiliskEnvironment(envname="APL-env",
     pkgname="APL",
-    packages=c("numpy==1.26.4", "pytorch==2.3.0")
+    packages=c("numpy==1.26.4", "pytorch==2.2.0")
 )
