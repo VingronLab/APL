f3ec53459e672bfbfd97fd89cfa34510127960bd
Clemens Kohl <kohl.clemens@gmail.com>
2024-10-28 21:37:33 +0100

    Revert "added mkl dependency"
    This reverts commit c9856d7fe5dc399b17d0d935c602feffb1160f3a.

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,12 +1,5 @@
 #' @importFrom basilisk BasiliskEnvironment
-APL_env <- BasiliskEnvironment(
-    envname = "APL-env",
-    pkgname = "APL",
-    packages = c(
-        "numpy==1.26.4",
-        "pytorch==2.3.0",
-        "libstdcxx-ng==14.2.0",
-        "libprotobuf==4.25.3",
-        "mkl==2023.2.0"
-    )
+APL_env <- BasiliskEnvironment(envname="APL-env",
+    pkgname="APL",
+    packages=c("numpy==1.26.4", "pytorch==2.3.0", "libstdcxx-ng==14.2.0", "libprotobuf==4.25.3")
 )
